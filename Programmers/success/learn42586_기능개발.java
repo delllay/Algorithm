1457dc93af59ba50824f6ef00ac7d335cec4af8b
yeon <delllllllay@gmail.com>
2020-09-14 16:46:52 +0900

    Update learn42586_기능개발.java

--- Programmers/success/learn42586_기능개발.java
+++ Programmers/success/learn42586_기능개발.java
@@ -1,22 +1,20 @@
-package success;
-
 // https://programmers.co.kr/learn/courses/30/lessons/42586
 // https://velog.io/@delay/programmers42586
 
 public class learn42586_기능개발 {
 	public static void main(String[] args) {
 		
-    int[] progresses = {93, 30, 55};
-    int[] speeds = {1, 30, 5};
-
+		int[] progresses = {93, 30, 55};
+		int[] speeds = {1, 30, 5};
+		
 		System.out.println(Arrays.toString(solution(progresses, speeds)));
 	}
 
 	 public int[] solution(int[] progresses, int[] speeds) {
-    Stack<Integer> stack = new Stack<Integer>();
+   		 Stack<Integer> stack = new Stack<Integer>();
 
-    for (int i = progresses.length - 1; i >= 0; i--)
-      stack.add((100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] > 0 ? 1 : 0));
+   		 for (int i = progresses.length - 1; i >= 0; i--)
+     			 stack.add((100 - progresses[i]) / speeds[i] + ((100 - progresses[i]) % speeds[i] > 0 ? 1 : 0));
 
     List<Integer> s = new ArrayList<Integer>();
 

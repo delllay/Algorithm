fc718b39dccae2d9c7cc876d84a748cd86af60c4
yeon <delllllllay@gmail.com>
2020-09-21 11:16:23 +0900

    Create learn68644_두_개_뽑아서_더하기.java

---
+++ Programmers/success/learn68644_두_개_뽑아서_더하기.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// https://programmers.co.kr/learn/courses/30/lessons/68644
+// https://velog.io/@delay/programmers68644
+
+public class learn68644_두_개_뽑아서_더하기 {
+	public static void main(String[] args) {
+		int[] numbers = {5,0,2,7};
+
+		System.out.println(Arrays.toString(solution(numbers)));
+	}
+
+	static public int[] solution(int[] numbers) {
+
+		List<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 0; i < numbers.length - 1; i++) {
+			for (int j = i + 1; j < numbers.length; j++) {
+				int c = numbers[i] + numbers[j];
+				if (list.indexOf(c) < 0)
+					list.add(c);
+			}
+		}
+
+		int[] answer = new int[list.size()];
+
+		for (int i = 0; i < list.size(); i++) {
+			answer[i] = list.get(i);
+		}
+
+		Arrays.sort(answer);
+		
+		return answer;
+	}
+}

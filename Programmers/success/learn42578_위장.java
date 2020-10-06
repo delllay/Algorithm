8f8d66932a41d16c9ae639f146e1e9b2dafcd3ed
yeon <delllllllay@gmail.com>
2020-10-06 13:37:58 +0900

    Create learn42578_위장.java

---
+++ Programmers/success/learn42578_위장.java
@@ -0,0 +1,31 @@
+// https://programmers.co.kr/learn/courses/30/lessons/42578
+// https://velog.io/@delay/programmers42578
+
+package programmers;
+
+import java.util.HashMap;
+
+public class learn42578_위장 {
+	public static void main(String[] args) {
+		String[][] clothes = { { "yellow_hat", "headgear" }, { "blue_sunglasses", "eyewear" },
+				{ "green_turban", "headgear" } };
+
+		System.out.print(solution(clothes));
+
+	}
+
+	public static int solution(String[][] clothes) {
+		HashMap<String, Integer> closet = new HashMap<String, Integer>();
+
+		for (int i = 0; i < clothes.length; i++) {
+			closet.put(clothes[i][1], closet.getOrDefault(clothes[i][1], 0) + 1);
+		}
+
+		int answer = 1;
+
+		for (int c : closet.values())
+			answer *= (c + 1);
+
+		return answer - 1;
+	}
+}

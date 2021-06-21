c01d3c5e7314750ac24d49b2c52299680eca27d1
yeon <delllllllay@gmail.com>
2021-06-21 21:26:44 +0900

    0621 알고리즘 공부
    kata7_vowels.java

--- Codewars/kata7_vowels.java
+++ Codewars/kata7_vowels.java
@@ -1,3 +1,9 @@
 public class kata7_vowels {
+	public static void main(String[] args) {
+		System.out.println(getCount("abracadabra"));
+	}
 
+	public static int getCount(String str) {
+		return str.replaceAll("(?i)[^aeiou]", "").replaceAll("(?!)[^aeiou]", "").length();
+	}
 }
